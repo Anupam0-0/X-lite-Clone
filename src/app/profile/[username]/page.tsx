8aaef500a028eded238ececa7c44f28306412d2f
Anupam0-0 <anupamchoudhary04@gmail.com>
2025-06-01 13:25:16 +0530

    mobile-view-profile fixed

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -8,7 +8,8 @@ import { notFound } from "next/navigation";
 import ProfilePageClient from "./ProfilePageClient";
 
 export async function generateMetadata({ params }: { params: { username: string } }) {
-  const user = await getProfileByUsername(params.username);
+  const awaitedParams = await params;
+  const user = await getProfileByUsername(awaitedParams.username);
   if (!user) return;
 
   return {
@@ -18,8 +19,8 @@ export async function generateMetadata({ params }: { params: { username: string 
 }
 
 async function ProfilePageServer({ params }: { params: { username: string } }) {
-  const {username} = await params
-  const user = await getProfileByUsername(username);
+  const awaitedParams = await params;
+  const user = await getProfileByUsername(awaitedParams.username);
 
   if (!user) notFound();
 
